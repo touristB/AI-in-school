1d9986d4ad9e7640634297047ee8cfdec5b161ae
touristB <66557228+touristB@users.noreply.github.com>
2020-09-21 20:22:21 +0200

    Add files via upload

--- MinimalesCodeBsp.py
+++ MinimalesCodeBsp.py
@@ -1,20 +1,22 @@
- # 21.9.2020
- # A. Schindler
- 
- import tensorflow as tf
- 
- # Erstellen der Trainingsdaten
- inputMuster = [1, 2, 4]
- outputMuster= [3, 6, 12]
- 
- # Aufbau des neuronalen Netzwerkes
- model = tf.keras.Sequential()
- model.add(tf.keras.layers.Dense(1, input_shape=[1]))
- model.compile(optimizer='sgd', loss='mean_squared_error')
- 
- # Trainieren des neuronalen Netzwerkes
- model.fit(inputMuster, outputMuster, epochs=1000)
- 
- # Testen des neuronalen Netzwerkes mit Testdaten hier 22
- testMuster = [22]
- print(model.predict(testMuster))
+# 21.9.2020
+# A. Schindler
+
+import tensorflow as tf                      
+
+# Erstellen der Trainingsdaten
+inputMuster = [1, 2, 4] 
+outputMuster= [3, 6, 12 ] 
+
+# Aufbau des neuronalen Netzwerkes
+model = tf.keras.Sequential()     
+model.add(tf.keras.layers.Dense(1, input_shape=[1]))
+model.compile(optimizer='sgd', loss='mean_squared_error')
+
+# trainieren des neuronalen Netzwerkes
+model.fit(inputMuster, outputMuster, epochs=1000)
+
+
+# Testen des neuronalen Netzwerkes mit Testdaten
+testMuster =  [22]
+print(model.predict(testMuster))
+
